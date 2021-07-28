194056d07ca67eb15be0a05bcd73f060e1bfe01f
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2021-07-28 12:57:33 -0500

    Initial commit

---
+++ generators/app/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+	welcome() {
+		this.log('Welcome to the Gulp and Sass Generator!');
+	}
+
+	writing() {
+		this.fs.copy(
+			this.templatePath('index.html'),
+			this.destinationPath('index.html')
+		);
+		this.fs.copy(
+			this.templatePath('gulpfile.js'),
+			this.destinationPath('gulpfile.js')
+		);
+		this.fs.copy(
+			this.templatePath('.gitignore'),
+			this.destinationPath('.gitignore')
+		);
+		this.fs.copy(
+			this.templatePath('package.json'),
+			this.destinationPath('package.json')
+		);
+		this.fs.copy(
+			this.templatePath('package-lock.json'),
+			this.destinationPath('package-lock.json')
+		);
+		this.fs.copy(this.templatePath('app'), this.destinationPath('app'));
+	}
+
+	end() {
+		this.log(' ');
+		this.log(
+			'Finished generating! Before running, make sure you have the Gulp CLI installed globally, and you do NOT have Gulp itself installed globally.'
+		);
+		this.log(' ');
+		this.log("If you're not sure how to do this, read this for more info:");
+		this.log('https://coder-coder.com/install-gulp-globally-on-windows/');
+		this.log(' ');
+		this.log(
+			"Once you have the Gulp CLI installed globally, you can run 'gulp' in this project directory to run Gulp!"
+		);
+		this.log(' ');
+	}
+};
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Gulp + Sass boilerplate\
+
+A Yeoman generator that creates a minimal Gulp and Sass front-end workflow.
+
+Based off of [Nando Moreirame's Gulp generator](https://github.com/nandomoreirame/generator-gulp-boilerplate) 🙏🏼
+
+## Instructions
+
+Install Yeoman globally: `npm install -g yo`
+
+Then install the Gulp + Sass boilerplate generator globally: `npm install -g generator-gulp-sass-boilerplate`
+
+Navigate to a directory you want to generate the files in, then run `yo gulp-sass-boilerplate`
+
+To run, you will need to have the Gulp CLI also installed globally:
+`npm install -g gulp-cli`
+
+Then in the project root, run the website by running `gulp`.
